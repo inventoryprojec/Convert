c98eb7093c3aae86ac009d9882445de5c22b1a06
molla <mollaruhul@outlook.com>
2023-11-21 18:39:42 -0500

    comments added in the main mathods

--- src/assignment4/Planet.java
+++ src/assignment4/Planet.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Planet {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+		Scanner in = new Scanner(System.in);// Scanner Object
 		System.out.println("Enter your weight on Earth");
-		double weightOnEarth = in.nextDouble();
+		double weightOnEarth = in.nextDouble();// getting user's input as double for weight on Earth
 		System.out.println("type your planet Name");
-		String choicePlanet = in.next();
+		String choicePlanet = in.next();// getting planet name in the form of String
 		double weightOnOtherPlanet = seeYourWeightOnOtherPlanet(weightOnEarth, choicePlanet);
 		System.out.println("Your Weight On " + choicePlanet + " is : " + weightOnOtherPlanet + " lbs");
 		in.close();
